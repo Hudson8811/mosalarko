1545c71a91f1c04cd5081fa57418387924397a00
Hudson8811 <hudson88@yandex.ru>
2021-02-18 15:49:11 +0300

    123

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-   // $("input[name='phone']").mask(" +7 (999) 999-99-99");
+	// $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
+	$('.js-freeoff-controller').click(function () {
+		var idx = $(this).index();
+		$('.freeoff-tab').eq(idx).addClass('freeoff-tab--active')
+			.siblings('.freeoff-tab--active').removeClass('freeoff-tab--active');
+		$(this).addClass('freeoff-tab-controls__item--active')
+			.siblings('.freeoff-tab-controls__item--active').removeClass('freeoff-tab-controls__item--active');
+	});
 });
-
